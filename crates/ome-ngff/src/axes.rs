1e42ebac2f36774d8cd2d1382a7fa30ae753439d
Lukas Herzberger <herzberger.lukas@gmail.com>
2022-05-23 18:44:05 -0400

    clean up axes

--- crates/ome-ngff/src/axes.rs
+++ crates/ome-ngff/src/axes.rs
@@ -1,6 +1,4 @@
-use std::fmt::{format, Formatter};
-use serde::{Serialize, Deserialize, Serializer, Deserializer};
-use serde::de::{MapAccess, Visitor};
+use serde::{Serialize, Deserialize};
 
 // https://ngff.openmicroscopy.org/0.4/#axes-md
 
@@ -180,103 +178,6 @@ impl From<MaybeTaggedAxis> for Axis {
         }
     }
 }
-
-/**
-impl Serialize for Axis {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
-        match self {
-            Axis::Space(value) => {
-                self::SpaceAxis::serialize(value, serializer)
-            }
-            Axis::Time(value) => {
-                self::TimeAxis::serialize(value, serializer)
-            }
-            Axis::Channel(value) => {
-                self::ChannelAxis::serialize(value, serializer)
-            }
-            Axis::Custom(value) => {
-                self::CustomAxis::serialize(value, serializer)
-            }
-        }
-    }
-}
-
-
-impl<'de>  Deserialize<'de> for Axis {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
-        struct AxisVisitor;
-        impl<'de> Visitor<'de> for AxisVisitor {
-            type Value = Axis;
-
-            fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
-                formatter.write_str("an Axis variant")
-            }
-
-            fn visit_map<V>(self, mut access: V) -> Result<Self::Value, V::Error> where V: MapAccess<'de> {
-                let mut name = "".to_string();
-                let mut axis_type: Option<String> = None;
-                let mut unit: Option<String> = None;
-                while let Some((key, value)) = access.next_entry()? {
-                    match key {
-                        "name" => {
-                            name = value;
-                        },
-                        "type" => {
-                            axis_type = Some(value);
-                        },
-                        "unit" => {
-                            unit = Some(value);
-                        },
-                        // ignore other keys
-                        _ => {}
-                    }
-                }
-
-                // todo: err on empty name
-
-                if axis_type.is_some() {
-                    match axis_type.unwrap().as_str() {
-                        "space" => {
-                            // todo: err on invalid unit
-                            Ok(Axis::Space(SpaceAxis::new(
-                                name.to_string(),
-                                None
-                            )))
-                        }
-                        "time" => {
-                            // todo: err on invalid unit
-                            Ok(Axis::Time(TimeAxis::new(
-                                name.to_string(),
-                                None
-                            )))
-                        },
-                        "channel" => {
-                            // todo: warn on lost unit
-                            Ok(Axis::Channel(ChannelAxis::new(
-                                name.to_string()
-                            )))
-                        },
-                        _ => {
-                            Ok(Axis::Custom(CustomAxis::new(
-                                name.to_string(),
-                                axis_type,
-                                unit
-                            )))
-                        }
-                    }
-                } else {
-                    Ok(Axis::Custom(CustomAxis::new(
-                        name,
-                        axis_type,
-                        unit
-                    )))
-                }
-            }
-        }
-        deserializer.deserialize_any(AxisVisitor)
-    }
-}
-*/
 
 #[cfg(test)]
 mod tests {
