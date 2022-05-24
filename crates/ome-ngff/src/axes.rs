36e78f943207f2db0f93f3a6c6a790ac14564858
Lukas Herzberger <herzberger.lukas@gmail.com>
2022-05-24 18:22:12 -0400

    add test cases

--- crates/ome-ngff/src/axes.rs
+++ crates/ome-ngff/src/axes.rs
@@ -1,8 +1,8 @@
-use serde::{Serialize, Deserialize};
+use serde::{Serialize, Deserialize, Serializer};
 
 // https://ngff.openmicroscopy.org/0.4/#axes-md
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum SpaceUnit {
     Angstrom,
@@ -33,7 +33,7 @@ pub enum SpaceUnit {
     Zettameter,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "lowercase")]
 pub enum TimeUnit {
     Attosecond,
@@ -61,10 +61,9 @@ pub enum TimeUnit {
     Zettasecond
 }
 
-
 // axis types
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct SpaceAxis {
     pub name: String,
 
@@ -81,7 +80,7 @@ impl SpaceAxis {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct TimeAxis {
     pub name: String,
 
@@ -98,7 +97,7 @@ impl TimeAxis {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct ChannelAxis {
     pub name: String,
 }
@@ -111,7 +110,7 @@ impl ChannelAxis {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct CustomAxis {
     pub name: String,
 
@@ -135,7 +134,7 @@ impl CustomAxis {
 
 // https://github.com/serde-rs/serde/issues/1799#issuecomment-624978919
 
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "lowercase")]
 enum TaggedAxis {
@@ -151,7 +150,7 @@ enum MaybeTaggedAxis {
     Untagged(CustomAxis),
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Deserialize)]
 #[serde(rename_all = "lowercase")]
 #[serde(from = "MaybeTaggedAxis")]
 #[serde(tag = "type")]
@@ -160,6 +159,26 @@ pub enum Axis {
     Time(TimeAxis),
     Channel(ChannelAxis),
     Custom(CustomAxis),
+}
+
+// custom Serialize implementation because otherwise CustomAxis instance without an axis_type get a "type": "custom"
+impl Serialize for Axis {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        match self {
+            Axis::Custom(value) => {
+                self::CustomAxis::serialize(value, serializer)
+            },
+            Axis::Space(value) => {
+                TaggedAxis::serialize(&TaggedAxis::Space(value.clone()), serializer)
+            }
+            Axis::Time(value) => {
+                TaggedAxis::serialize(&TaggedAxis::Time(value.clone()), serializer)
+            }
+            Axis::Channel(value) => {
+                TaggedAxis::serialize(&TaggedAxis::Channel(value.clone()), serializer)
+            }
+        }
+    }
 }
 
 impl From<MaybeTaggedAxis> for Axis {
@@ -179,21 +198,305 @@ impl From<MaybeTaggedAxis> for Axis {
 
 #[cfg(test)]
 mod tests {
-    // todo:
-    //  - space without unit
-    //  - space with unit
-    //  - time without unit
-    //  - time with unit
-    //  - channel without unit
-    //  - channel with unit (should be same as without)
-    //  - custom axis without unit and type
-    //  - custom axis with unit and without type
-    //  - custom axis without unit and with type
-    //  - axis without name should throw
+    use serde_json::{from_str, to_string, Value};
+    use super::*;
+
+    fn space_no_unit_json() -> String {
+        r#"{"name": "x", "type": "space"}"#.to_string()
+    }
+
+    fn space_json() -> String {
+        r#"{"name": "y", "type": "space", "unit": "millimeter"}"#.to_string()
+    }
+
+    fn space_no_unit() -> SpaceAxis {
+        SpaceAxis::new("x".to_string(), None)
+    }
+
+    fn space() -> SpaceAxis {
+        SpaceAxis::new("y".to_string(), Some(SpaceUnit::Millimeter))
+    }
+
+    fn time_no_unit_json() -> String {
+        r#"{"name": "t", "type": "time"}"#.to_string()
+    }
+
+    fn time_json() -> String {
+        r#"{"name": "t", "type": "time", "unit": "millisecond"}"#.to_string()
+    }
+
+    fn time_no_unit() -> TimeAxis {
+        TimeAxis::new("t".to_string(), None)
+    }
+
+    fn time() -> TimeAxis {
+        TimeAxis::new("t".to_string(), Some(TimeUnit::Millisecond))
+    }
+
+    fn channel_no_unit_json() -> String {
+        r#"{"name": "c", "type": "channel"}"#.to_string()
+    }
+
+    fn channel_json() -> String {
+        r#"{"name": "c", "type": "channel", "unit": "should get ingnored"}"#.to_string()
+    }
+
+    fn channel() -> ChannelAxis {
+        ChannelAxis::new("c".to_string())
+    }
+
+    fn custom_no_unit_no_type_json() -> String {
+        r#"{"name": "foo"}"#.to_string()
+    }
+
+    fn custom_no_unit_json() -> String {
+        r#"{"name": "foo", "type": "bar"}"#.to_string()
+    }
+
+    fn custom_no_type_json() -> String {
+        r#"{"name": "foo", "unit": "bar"}"#.to_string()
+    }
+
+    fn custom_json() -> String {
+        r#"{"name": "foo", "type": "bar", "unit": "baz"}"#.to_string()
+    }
+
+    fn custom_no_unit_no_type() -> CustomAxis {
+        CustomAxis::new("foo".to_string(), None, None)
+    }
+
+    fn custom_no_unit() -> CustomAxis {
+        CustomAxis::new("foo".to_string(), Some("bar".to_string()), None)
+    }
+
+    fn custom_no_type() -> CustomAxis {
+        CustomAxis::new("foo".to_string(), None, Some("bar".to_string()))
+    }
+
+    fn custom() -> CustomAxis {
+        CustomAxis::new("foo".to_string(), Some("bar".to_string()), Some("baz".to_string()))
+    }
+
+    fn space_invalid_json() -> String {
+        r#"{"type": "space"}"#.to_string()
+    }
+
+    fn time_invalid_json() -> String {
+        r#"{"type": "time"}"#.to_string()
+    }
+
+    fn channel_invalid_json() -> String {
+        r#"{"type": "channel"}"#.to_string()
+    }
+
+    fn custom_invalid_json() -> String {
+        r#"{"type": "foo"}"#.to_string()
+    }
+
+    fn axes_json(exclude_channel_unit: bool) -> String {
+        format!(
+            "[{},{},{},{},{},{},{},{},{},{}]",
+            space_no_unit_json(),
+            space_json(),
+            time_no_unit_json(),
+            time_json(),
+            channel_no_unit_json(),
+            if exclude_channel_unit { channel_no_unit_json() } else { channel_json() },
+            custom_no_unit_no_type_json(),
+            custom_no_unit_json(),
+            custom_no_type_json(),
+            custom_json()
+        )
+    }
+
+    fn axes() -> Vec<Axis> {
+        vec![
+            Axis::Space(space_no_unit()),
+            Axis::Space(space()),
+            Axis::Time(time_no_unit()),
+            Axis::Time(time()),
+            Axis::Channel(channel()),
+            Axis::Channel(channel()),
+            Axis::Custom(custom_no_unit_no_type()),
+            Axis::Custom(custom_no_unit()),
+            Axis::Custom(custom_no_type()),
+            Axis::Custom(custom()),
+        ]
+    }
 
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn serialize_space_without_unit() {
+        assert_eq!(
+            from_str::<Value>(&to_string(&Axis::Space(space_no_unit())).unwrap()).unwrap(),
+            from_str::<Value>(&space_no_unit_json()).unwrap()
+        );
+    }
+
+    #[test]
+    fn deserialize_space_without_unit() {
+        assert_eq!(
+            from_str::<SpaceAxis>(&space_no_unit_json()).unwrap(),
+            space_no_unit()
+        );
+    }
+
+    #[test]
+    fn serialize_space_with_unit() {
+        assert_eq!(
+            from_str::<Value>(&to_string(&&Axis::Space(space())).unwrap()).unwrap(),
+            from_str::<Value>(&space_json()).unwrap()
+        );
+    }
+
+    #[test]
+    fn deserialize_space_with_unit() {
+        assert_eq!(
+            from_str::<SpaceAxis>(&space_json()).unwrap(),
+            space()
+        );
+    }
+
+    #[test]
+    fn serialize_time_without_unit() {
+        assert_eq!(
+            from_str::<Value>(&to_string(&Axis::Time(time_no_unit())).unwrap()).unwrap(),
+            from_str::<Value>(&time_no_unit_json()).unwrap()
+        );
+    }
+
+    #[test]
+    fn deserialize_time_without_unit() {
+        assert_eq!(
+            from_str::<TimeAxis>(&time_no_unit_json()).unwrap(),
+            time_no_unit()
+        );
+    }
+
+    #[test]
+    fn serialize_time_with_unit() {
+        assert_eq!(
+            from_str::<Value>(&to_string(&Axis::Time(time())).unwrap()).unwrap(),
+            from_str::<Value>(&time_json()).unwrap()
+        );
+    }
+
+    #[test]
+    fn deserialize_time_with_unit() {
+        assert_eq!(
+            from_str::<TimeAxis>(&time_json()).unwrap(),
+            time()
+        );
+    }
+
+    #[test]
+    fn serialize_channel_without_unit() {
+        assert_eq!(
+            from_str::<Value>(&to_string(&Axis::Channel(channel())).unwrap()).unwrap(),
+            from_str::<Value>(&channel_no_unit_json()).unwrap()
+        );
+    }
+
+    #[test]
+    fn deserialize_channel_without_unit() {
+        assert_eq!(
+            from_str::<ChannelAxis>(&channel_no_unit_json()).unwrap(),
+            channel()
+        );
+    }
+
+    #[test]
+    fn deserialize_channel_with_unit() {
+        assert_eq!(
+            from_str::<ChannelAxis>(&channel_json()).unwrap(),
+            channel()
+        );
+    }
+
+    #[test]
+    fn serialize_custom_without_type_without_unit() {
+        assert_eq!(
+            from_str::<Value>(&to_string(&Axis::Custom(custom_no_unit_no_type())).unwrap()).unwrap(),
+            from_str::<Value>(&custom_no_unit_no_type_json()).unwrap()
+        );
+    }
+
+    #[test]
+    fn deserialize_custom_without_type_without_unit() {
+        assert_eq!(
+            from_str::<CustomAxis>(&custom_no_unit_no_type_json()).unwrap(),
+            custom_no_unit_no_type()
+        );
+    }
+
+    #[test]
+    fn serialize_custom_without_type_with_unit() {
+        assert_eq!(
+            from_str::<Value>(&to_string(&Axis::Custom(custom_no_type())).unwrap()).unwrap(),
+            from_str::<Value>(&custom_no_type_json()).unwrap()
+        );
+    }
+
+    #[test]
+    fn deserialize_custom_without_type_with_unit() {
+        assert_eq!(
+            from_str::<CustomAxis>(&custom_no_type_json()).unwrap(),
+            custom_no_type()
+        );
+    }
+
+    #[test]
+    fn serialize_custom_with_type_with_unit() {
+        assert_eq!(
+            from_str::<Value>(&to_string(&Axis::Custom(custom_no_unit())).unwrap()).unwrap(),
+            from_str::<Value>(&custom_no_unit_json()).unwrap()
+        );
+    }
+
+    #[test]
+    fn deserialize_custom_with_type_with_unit() {
+        assert_eq!(
+            from_str::<CustomAxis>(&custom_no_unit_json()).unwrap(),
+            custom_no_unit()
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn deserialize_invalid_space() {
+        from_str::<Axis>(&space_invalid_json()).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn deserialize_invalid_time() {
+        from_str::<Axis>(&time_invalid_json()).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn deserialize_invalid_channel() {
+        from_str::<Axis>(&channel_invalid_json()).unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn deserialize_invalid_custom() {
+        from_str::<Axis>(&custom_invalid_json()).unwrap();
+    }
+
+    #[test]
+    fn serialize_axes() {
+        assert_eq!(
+            from_str::<Value>(&to_string(&axes()).unwrap()).unwrap(),
+            from_str::<Value>(&axes_json(true)).unwrap()
+        );
+    }
+
+    #[test]
+    fn deserialize_axes() {
+        assert_eq!(
+            from_str::<Vec<Axis>>(&axes_json(false)).unwrap(),
+            axes()
+        );
     }
 }
